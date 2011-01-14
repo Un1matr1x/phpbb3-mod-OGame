5eb5a968bff751266e70d659fea8f9d5e37c77b5
Un1matr1x <admin@un1matr1x.de>
2011-01-14 19:19:00 +0100

    ModVersions-Check file added to support http://www.phpbb.com/customise/db/mod/mod_version_check/
    Signed-off-by: Un1matr1x <admin@un1matr1x.de>

---
+++ root/adm/mods/ogame_mod_version.php
@@ -0,0 +1,34 @@
+<?php
+/**
+*
+* @package acp
+* @version $Id: mod_version_check_version.php 51 2007-10-30 04:40:42Z Handyman $
+* @copyright (c) 2007 StarTrekGuide
+* @license http://opensource.org/licenses/gpl-license.php GNU Public License
+*
+*/
+
+/**
+* @package mod_version_check
+*/
+
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+class ogame_mod_version
+{
+	function version()
+	{
+		return array(
+			'author'	=> 'Un1matr1x',
+			'title'		=> 'OGame-Mod',
+			'tag'		=> 'ogame_mod',
+			'version'	=> '0.0.1',
+			'file'		=> array('un1matr1x.de', 'check', 'ogm.xml'),
+		);
+	}
+}
+
+?>
