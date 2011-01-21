777b9dbb4dccd6c48759bbb6a24d00a52b572b78
Un1matr1x <admin@un1matr1x.de>
2011-01-21 23:13:18 +0100

    ScannBB-Evolution added
    Signed-off-by: Un1matr1x <admin@un1matr1x.de>

--- root/includes/functions_ogame.php
+++ root/includes/functions_ogame.php
@@ -41,4 +41,44 @@ $search_kb[9] 	= '#[^"]http://kb.un1matr1x.de/kb\.php\?show=([0-9]+)<#'; //?show
 	}
 	
 	$text = preg_replace($search_kb, $replace_kb, $text);
+
+if(!function_exists('ogame_scan_de'))
+{
+	function ogame_scan_de($treffer)
+	{
+            $txt=$treffer[0];
+            if(substr_count($txt,"<br />")>0) $rows=split("<br />",$txt);
+            else $rows=split("\n",$txt);
+            $rowsold=$rows;
+            $countrows=count($rows);
+            $p1=preg_match("/(Metall).{1,}(Kristall)/",$rows[1]);
+            $p2=preg_match("/(Deuterium).{1,}(Energie)/",$rows[2]);
+            if ($countrows>3 && $countrows<42 && $p1 && $p2){
+                for ($i=0; $i<$countrows; $i++){
+                    $rows[$i]=preg_replace("/(Rohstoffe auf .*)\[([1-9]{1,2}:[0-9]{1,3}:[0-9]{1,2})\](.*)/",'<tr><th class="area" colspan="6">\\1[<span class="coords">\\2</span>]\\3</th></tr>',$rows[$i]);
+					$rows[$i]=preg_replace("/(Dein Sondenscan .*)\./",'<tr><th class="area" colspan="6">Aktivit'.utf8_encode("ä").'t</th></tr><tr><td colspan="4" class="activity">\\1</td></tr>',$rows[$i]);
+                    $rows[$i]=preg_replace("/(Flotten)/",'<tr><th class="area" colspan="6">\\1</th></tr>',$rows[$i]);
+                    $rows[$i]=preg_replace("/(Verteidigung)/",'<tr><th class="area" colspan="6">\\1</th></tr>',$rows[$i]);
+                    $rows[$i]=preg_replace("/(Geb".utf8_encode("ä")."ude)/",'<tr><th class="area" colspan="6">\\1</th></tr>',$rows[$i]);
+                    $rows[$i]=preg_replace("/(Geb&auml;ude)/",'<tr><th class="area" colspan="6">\\1</th></tr>',$rows[$i]);
+                    $rows[$i]=preg_replace("/(Forschung)\b/",'<tr><th class="area" colspan="6">\\1</th></tr>',$rows[$i]);
+                    $rows[$i]=preg_replace("/(Chance)(.*)/",'<tr><th class="defense" colspan="4">\\1\\2</th></tr>',$rows[$i]);
+                    if($rowsold[$i]==$rows[$i]){
+                        preg_match_all("/(-?(?:\.?\d)+)/",$rows[$i],$dots, PREG_SET_ORDER);
+                        if(count($dots)==2) $rows[$i]=preg_replace("/([A-Za-z:".utf8_encode("ßöäü")."\t&; ]+)(-?(?:\.?\d)+)([A-Za-z:".utf8_encode("ßöäü")."\t&; ]+)(-?(?:\.?\d)+)/",'<tr><td class="key">\\1</td><td class="value">\\2</td><td class="key">\\3</td><td class="value">\\4</td></tr>',$rows[$i]);
+                        if(count($dots)==1) $rows[$i]=preg_replace("/([A-Za-z:".utf8_encode("ßöäü")."\t&; ]+)(-?(?:\.?\d)+)/",'<tr><td class="key" colspan="3">\\1</td><td class="value" colspan="1">\\2</td></tr>',$rows[$i]);
+                    }
+                }
+				//Aktivität eliminieren
+				$rows[3] = '';
+                $txt=join('',$rows);
+                $txt='<div class="textWrapper"><div class="node"><table cellpadding="0" cellspacing="0" class="material spy">'.$txt.'</table></div></div>';
+            }				
+        return $txt;
+        }
+}
+
+$scanpattern="/Rohstoffe.auf(.){1,25}\[(.+?)Spionageabwehr(.+?)\%/s";
+$text = preg_replace_callback($scanpattern,'ogame_scan_de',$text);
+
 ?>
