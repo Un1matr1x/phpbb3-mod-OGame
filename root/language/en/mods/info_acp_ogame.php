72fefcdcae6ff41ca772709c7da76aa960a9d3e3
Un1matr1x <admin@un1matr1x.de>
2011-01-22 09:50:29 +0100

    new ACP-Settings added and language vars added (settings won't change a thing by now - that's just the design)
    Signed-off-by: Un1matr1x <admin@un1matr1x.de>

--- root/language/en/mods/info_acp_ogame.php
+++ root/language/en/mods/info_acp_ogame.php
@@ -39,28 +39,40 @@ if (empty($lang) || !is_array($lang))
 
 $lang = array_merge($lang, array(
 
-	'CR_HOSTING'					=> 'CR-Hosting',
-	'CR_HOSTING_EXPLAIN'			=> 'Enter your CR-Hosting-User-ID.<br /> In case of  http://kb.un1matr1x.de/memberlist.php?mode=viewprofile&u=<b>2</b> choose 2',
-	'CR_HOSTING_S_EXPLAIN'			=> 'Show the CR-Hosting-ID & Signature',
+	'CR_HOSTING'						=> 'CR-Hosting',
+	'CR_HOSTING_EXPLAIN'				=> 'Enter your CR-Hosting-User-ID.<br /> In case of  http://kb.un1matr1x.de/memberlist.php?mode=viewprofile&u=<b>2</b> choose 2',
+	'CR_HOSTING_MOUSEOVER_SHOW'			=> 'CR-Hosting-Sigi on miniprofile',
+	'CR_HOSTING_MOUSEOVER_SHOW_EXPLAIN' => 'display the CR-Hosting-Sigi on mouseover the CR-Hosting-Icon',
+	'CR_HOSTING_PROFILE_SHOW'			=> 'allow CR-Hosting-Sigi on profile',
+	'CR_HOSTING_PROFILE_SHOW_EXPLAIN'	=> 'show the CR-Hosting-Sigi underneath the signature or contact data on the memberlist viewprofile tab',
+	'CR_HOSTING_S'						=> 'Allow CR-Hosting connection',
+	'CR_HOSTING_S_EXPLAIN'				=> 'activate CR-Hosting-UCP settings and show the CR-Hosting-User-ID on (mini-)profiles',
 
-	'IMG_ICON_CONTACT_MMOGAME'		=> 'Gameforge',
-	'IMG_ICON_CONTACT_CR_HOSTING'	=> 'CR-Hosting',
+	'IMG_ICON_CONTACT_MMOGAME'			=> 'Gameforge',
+	'IMG_ICON_CONTACT_CR_HOSTING'		=> 'CR-Hosting',
 
-	'MMOGAME'						=> 'Gameforge',
-	'MMOGAME_EXPLAIN'				=> 'Enter your Gameforge-ID.<br /> In case of http://gameforge.com/profile/show/Nickname-<b>47634</b> choose 47634',
-	'MMOGAME_S_EXPLAIN'				=> 'Show the Gameforge-ID & G-Card',
+	'MMOGAME'							=> 'Gameforge',
+	'MMOGAME_EXPLAIN'					=> 'Enter your Gameforge-ID.<br /> In case of http://gameforge.com/profile/show/Nickname-<b>47634</b> choose 47634',
+	'MMOGAME_MOUSEOVER_SHOW'			=> 'G-Card on miniprofile',
+	'MMOGAME_MOUSEOVER_SHOW_EXPLAIN'	=> 'display the G-Card on mouseover the G-Icon',
+	'MMOGAME_PROFILE_SHOW'				=> 'allow G-Card on profile',
+	'MMOGAME_PROFILE_SHOW_EXPLAIN'		=> 'show the G-Card underneath the signature or contact data on the memberlist viewprofile tab',
+	'MMOGAME_S'							=> 'Allow Gameforge Portal connection',
+	'MMOGAME_S_EXPLAIN'					=> 'activate G-UCP settings and show the Gameforge-ID on (mini-)profiles',
+	'MMOGAME_STYLE'						=> 'Standard G-Card Design',
+	'MMOGAME_STYLE_EXPLAIN'				=> 'Standart G-Card has the dimensions 225px x 150px , non-Standart would be the slim on with 452px x 102px',
 
-	'OGAME'							=> 'OGame',
-	'OGAME_CONFIG'					=> 'Configuration',
-	'OGAME_SAVED'					=> 'saved setup',
+	'OGAME'								=> 'OGame',
+	'OGAME_CONFIG'						=> 'Configuration',
+	'OGAME_SAVED'						=> 'saved setup',
 	
-	'UCP_MMOGAME'					=> 'MMOGame-ID',
-	'UCP_CR_HOSTING'				=> 'CR-Hosting-Profile-ID',
+	'UCP_MMOGAME'						=> 'MMOGame-ID',
+	'UCP_CR_HOSTING'					=> 'CR-Hosting-Profile-ID',
 	
-	'VIEW_MMOGAME'					=> 'visit Gameforge-Profile',
-	'VIEW_CR_HOSTING'				=> 'visit CR-Hosting-Profile',
+	'VIEW_MMOGAME'						=> 'visit Gameforge-Profile',
+	'VIEW_CR_HOSTING'					=> 'visit CR-Hosting-Profile',
 	
-	'WRONG_DATA_MMOGAME'			=> 'Your Gameforge-ID should only contain Integer.',
-	'WRONG_DATA_CR_HOSTING'			=> 'Your CR-Hosting-User-ID should only contain Integer.',
+	'WRONG_DATA_MMOGAME'				=> 'Your Gameforge-ID should only contain Integer.',
+	'WRONG_DATA_CR_HOSTING'				=> 'Your CR-Hosting-User-ID should only contain Integer.',
 ));
 ?>
